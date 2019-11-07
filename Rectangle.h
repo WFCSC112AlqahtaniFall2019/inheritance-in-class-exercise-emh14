08770dd0dafd15c5ff8e46ab12722d9c0c42c892
ethanharrison <harrem18@wfu.edu>
2019-11-07 14:56:22 -0500

    Create Rectangle and Triangle class cpp and h files

---
+++ Rectangle.h
@@ -0,0 +1,17 @@
+//
+// Created by Ethan Harrison on 11/7/19.
+//
+
+#ifndef INHERTANCE_EXERCISE_RECTANGLE_H
+#define INHERTANCE_EXERCISE_RECTANGLE_H
+
+
+class Rectangle {
+public:
+
+protected:
+
+};
+
+
+#endif //INHERTANCE_EXERCISE_RECTANGLE_H
---
+++ Triangle.h
@@ -0,0 +1,17 @@
+//
+// Created by Ethan Harrison on 11/7/19.
+//
+
+#ifndef INHERTANCE_EXERCISE_TRIANGLE_H
+#define INHERTANCE_EXERCISE_TRIANGLE_H
+
+class Triangle {
+public:
+
+protected:
+
+};
+
+
+
+#endif //INHERTANCE_EXERCISE_TRIANGLE_H
